d17454563f0918d130607580cee86c9006173d4a
Gabriel Garcia <gabito1966@gmail.com>
2024-08-20 18:05:58 -0300

    cambio bg home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,8 @@
 import Image from "next/image";
 
-function HomePage() {
+export default function Home() {
   return (
-    <div className="justify-center mx-auto  p-20 text-center bg-slate-600">
+    <div className="justify-center mx-auto  p-20 text-center bg-slate-800">
       <div className="font-bold text-6xl">
         <h1>Polo It</h1>
       </div>
@@ -12,5 +12,3 @@ function HomePage() {
     </div>
   );
 }
-
-export default HomePage
