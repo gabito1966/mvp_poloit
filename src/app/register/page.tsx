c4d71914107507e80767c8f72de110afda020c5c
Gabriel Garcia <gabito1966@gmail.com>
2024-08-22 13:32:32 -0300

    cambio de colores botones alumnos y mentores

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -4,12 +4,12 @@ function Register() {
     return (
         <div className="container mx-auto justify-center flex min-h-full px-6 py-20 lg:px-8 h-screen gap-20 bg-slate-200">
             <Link href='/register/alumnos'>
-                <button type="button" className="bg-gray-300 hover:bg-gray-500 text-white w-40 rounded-md p-3">
+                <button type="button" className="bg-gray-500 hover:bg-gray-900 text-white w-40 rounded-md p-3">
                     Registro de alumnos
                 </button>
             </Link>
             <Link href='/register/mentores'>
-                <button type="button" className="bg-gray-300 hover:bg-gray-500 text-white w-40 rounded-md p-3">
+                <button type="button" className="bg-gray-500 hover:bg-gray-900 text-white w-40 rounded-md p-3">
                     Registro de mentores
                 </button>
             </Link>
