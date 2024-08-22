3a9b255a4c0793ff1cb4c63696ad799b76acd6a5
javier <espindolajavier2013@gmail.com>
2024-08-21 21:23:44 -0300

    se uso la libraria zod para validar

--- src/app/api/login/route.ts
+++ src/app/api/login/route.ts
@@ -2,6 +2,7 @@ import { NextResponse } from "next/server";
 import { sql } from "@vercel/postgres";
 import { comparePassword, hashPassword, JWTCreate } from "@/lib/utils";
 import { cookies } from "next/headers";
+import { z } from "zod";
 
 export async function GET(request: Request) {
   return NextResponse.json({ message: "hola mundo" });
@@ -11,6 +12,11 @@ export interface UserLogin {
   email: string;
   password: string;
 }
+
+export type DataZ = {
+  email: string;
+  password: string;
+};
 
 export interface UserLoginResponse {
   success: boolean;
@@ -26,69 +32,73 @@ export type Administrador = {
   contrasena: string;
 };
 
+const FormSchema = z.object({
+  email: z
+    .string()
+    .email("Debe ser un email válido")
+    .min(6, "el email de tener al menos 6 caracteres"),
+  password: z.string().min(6, "La contraseña debe tener al menos 6 caracteres"),
+});
+
+const CreateLogin = FormSchema.omit({});
+
 export async function POST(request: Request) {
-  const data = (await request.json()) as UserLogin;
-  const { email, password } = data;
+  const body = (await request.json()) as UserLogin;
 
-  if (!email || !password) {
+  try {
+    const { email, password } = CreateLogin.parse({
+      email: body.email,
+      password: body.password,
+    });
+
+    const { rows } =
+      await sql<Administrador>`SELECT * from administradores WHERE email = ${email}`;
+
+    if (rows.length === 0) {
+      const res: UserLoginResponse = {
+        success: false,
+        message: "El administrador no esta registrado",
+      };
+
+      return NextResponse.json(res, { status: 500 });
+    }
+
+    const user = rows[0];
+    const match = await comparePassword(password, user.contrasena);
+
+    if (!match) {
+      const res: UserLoginResponse = {
+        success: false,
+        message: "Error en el email o la contraseña",
+      };
+
+      return NextResponse.json(res, { status: 500 });
+    }
+
     const res: UserLoginResponse = {
-      success: false,
-      message: "Falta el email o la contraseña",
+      success: true,
+      userData: user,
+      message: "Login Exitoso",
     };
 
-    return NextResponse.json(res, { status: 400 });
-  }
+    const token: String = await JWTCreate(user);
 
-  const { rows } =
-    await sql<Administrador>`SELECT * from administradores WHERE email = ${email}`;
+    return NextResponse.json(res, { status: 200 });
+  } catch (error) {
+    if (error instanceof z.ZodError) {
+      const res = {
+        success: false,
+        message: error.errors.map((err) => err.message).join(", "),
+      };
 
-  if (rows.length === 0) {
-    const res: UserLoginResponse = {
+      return NextResponse.json(res, { status: 400 });
+    }
+
+    const res = {
       success: false,
-      message: "El administrador no esta registrado",
+      message: "error en la base de datos",
     };
 
     return NextResponse.json(res, { status: 500 });
   }
-
-  const user = rows[0];
-  const match = await comparePassword(password, user.contrasena);
-
-  if (!match) {
-    //return new Response("Usuario o password incorrectos", { status: 401 });
-    const res: UserLoginResponse = {
-      success: false,
-      message: "Error en el email o la contraseña",
-    };
-
-    return NextResponse.json(res, { status: 500 });
-  }
-
-  const res: UserLoginResponse = {
-    success: true,
-    userData: user,
-    message: "Login Exitoso",
-  };
-
-  const expiresAt = new Date(Date.now() + 7 * 24 * 60 * 60 * 1000);
-
-  const token: String = await JWTCreate(user);
-
-  cookies().set("JWTCookie", token.toString(), {
-    httpOnly: true,
-    secure: true,
-    expires: expiresAt,
-    sameSite: "lax",
-    path: "/",
-  });
-
-  // NextResponse.next().cookies.set("JWTCookie", token.toString(), {
-  //   httpOnly: true,
-  //   secure: true,
-  //   expires: expiresAt,
-  //   sameSite: "lax",
-  //   path: "/",
-  // });
-
-  return NextResponse.json(res, { status: 200 });
 }
