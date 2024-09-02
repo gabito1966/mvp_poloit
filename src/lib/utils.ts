b09468d068258678bc9914a0512e8e7e5a49eede
Gabriel Garcia <gabito1966@gmail.com>
2024-09-02 17:37:54 -0300

    ajuste lib/utils

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,69 +1,70 @@
 import { Administrador } from "@/app/api/login/route";
-import bcrypt from "bcrypt";
 import { SignJWT, jwtVerify } from "jose";
-
-export const generateHash = async (cad: string) => {
-  //   const salt = await bcrypt.genSalt();
-  const hashedCad = await bcrypt.hash(`${cad}`, 10);
-  return hashedCad;
-};
-
-export const comparePassword = async (
-  password: string,
-  hashedPassword: string
-) => {
-  return await bcrypt.compare(password, hashedPassword);
-};
-
-const secretKey = process.env.SESSION_SECRET;
-const encodedKey = new TextEncoder().encode(secretKey);
 
 export async function JWTCreate(payload: Administrador) {
   return new SignJWT(payload)
     .setProtectedHeader({ alg: "HS256" })
     .setIssuedAt()
     .setExpirationTime("7d")
-    .sign(encodedKey);
+    .sign(getJwtSecretKey());
+}
+
+export function getJwtSecretKey() {
+  const secret = process.env.SESSION_SECRET;
+
+  if (!secret) {
+    throw new Error("JWT Secret key is not set");
+  }
+
+  const enc: Uint8Array = new TextEncoder().encode(secret);
+  return enc;
 }
 
 export async function JWTValidate(session: string | undefined = "") {
   try {
-    const { payload } = await jwtVerify(session, encodedKey, {
+    const { payload } = await jwtVerify(session, getJwtSecretKey(), {
       algorithms: ["HS256"],
     });
     return payload;
   } catch (error) {
-    console.log("Falló la validacion del JWT");
+    console.log("fallo la validacion de JWT");
     return null;
   }
 }
 
-export function getErrorMessageFromCode(
-  code: string,
-  contraint: string
-): string {
-  let mensaje = "";
+export function getErrorMessageFromCode(error: any) {
+  try {
+    let mensaje = "";
 
-  if ((code = "23505")) {
-    const arr = contraint.split("_");
+    console.log(error);
 
-    mensaje = arr.splice(1, arr.length).join("");
+    if ((error.code = "23505")) {
+      if (!error.constraint) {
+        return "Error en sintaxis de la consulta a la base de datos";
+      }
+
+      const arr = error.constraint.split("_");
+
+      mensaje = arr.splice(1, arr.length - 2).join("");
+    }
+
+    const errorMessagesCodes: { [code: string]: string } = {
+      "23505": `${mensaje} ya existe`,
+      "23502": "No se puede insertar un valor NULL en esta columna",
+      "23503": "La clave externa no es válida",
+      "22001": "Valor demasiado largo para la columna",
+      "22008": "Valor no válido para la columna",
+      "22012": "Valor NULL no permitido en la columna",
+      "22019": "Valor no válido para la columna",
+      "28000": "Error de autenticación",
+      "3D000": "Error de sintaxis en la consulta",
+      "42P01": "Error de sintaxis en la definición de la tabla",
+      "42P02": "Error de sintaxis en la definición de la columna",
+    };
+    return errorMessagesCodes[error.code] || "Error en la base de datos";
+  } catch (error) {
+    return "Error en la base de datos";
   }
-
-  const errorMessagesCodes: { [code: string]: string } = {
-    "23505": `${mensaje} ya existe`,
-    "23502": "No se puede insertar un valor NULL en esta columna",
-    "23503": "La clave externa no es válida",
-    "22001": "Valor demasiado largo para la columna",
-    "22008": "Valor no válido para la columna",
-    "22012": "Valor NULL no permitido en la columna",
-    "22019": "Valor no válido para la columna",
-    "28000": "Error de autenticación",
-    "3D000": "Error de sintaxis en la consulta",
-    "42P01": "Error de sintaxis en la definición de la tabla",
-    "42P02": "Error de sintaxis en la definición de la columna",
-  };
-  return errorMessagesCodes[code] || "Error en la base de datos";
 }
 
 export function createResponse(
