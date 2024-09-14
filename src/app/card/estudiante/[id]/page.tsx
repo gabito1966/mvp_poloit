aebbd44c5d697fa6e93686cb8913f44350c36f26
Gabriel Garcia <gabito1966@gmail.com>
2024-09-14 20:24:46 -0300

    Agregado de ongs en home

--- src/app/card/estudiante/[id]/page.tsx
+++ src/app/card/estudiante/[id]/page.tsx
@@ -12,7 +12,7 @@ export default async function EstudianteCard({
 
     return (
         <section className="container">
-            <h1 className="text-4xl text-center font-bold p-2 mt-10">Card de Estudiante {estudiante.nombre} {estudiante.apellido}</h1>
+            <h1 className="text-4xl text-center font-bold p-2 mt-10">Card del Estudiante {estudiante.nombre} {estudiante.apellido}</h1>
 
             <div className="w-full m-5 max-h-124 flex-col p-1  md:p-12 text-black border-2 border-black bg-neutral-200">
                 {<div className="grid grid-cols-1 md:grid-cols-2">
@@ -37,7 +37,7 @@ export default async function EstudianteCard({
                         <p className="bg-transparent text-base mb-6">{estudiante.tecnologias[0].nombre}</p>
                     </div>
                     <div className="flex-auto">
-                        <h4 className="block font-semibold mb-2 text-md">Tecnologías tipo:</h4>
+                        <h4 className="block font-semibold mb-2 text-md">Tipo Tecnologia:</h4>
                         <p className="bg-transparent text-base mb-6">{estudiante.tecnologias[0].tipo}</p>
                     </div>
 
@@ -47,7 +47,8 @@ export default async function EstudianteCard({
                     </div>
                     <div className="flex-auto">
                         <h4 className="block font-semibold mb-2 text-md">Estado:</h4>
-                        <p className="bg-transparent text-base mb-6">{estudiante.estado}</p>
+                        <p className="bg-transparent text-base mb-6">{estudiante.estado ? 'Activo' : 'Inactivo'}</p>
+
                     </div>
 
                 </div>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,12 +2,30 @@ import Image from "next/image";
 
 export default async function Home() {
   return (
-    <section className="justify-center mx-auto  text-center text-black ">
+    <section className="container  text-black ">
       <div className="font-bold text-6xl">
-        <h1>Polo It</h1>
+        <h1 className="text-center">Polo It</h1>
       </div>
       <div className="py-20 flex items-center justify-center">
         <Image src='/logo-polo-it.png' alt="logo" width={375} height={150} className="object-center" />
+      </div>
+      <div className="container border-2 border-gray-800 rounded-lg w-full h-148">
+        <h2 className="font-bold text-2xl text-center w-full underline">Organizaciones Participantes</h2>
+
+        <div className="flex flex-wrap wrap gap-5 justify-around">
+          <div className="py-20 flex ">
+            <Image src='/logo-integrar.png' alt="logo integrar" width={150} height={50} className="object-center" />
+          </div>
+          <div className="py-20 flex  ">
+            <Image src='/logo-empujar.png' alt="logo empujar" width={150} height={50} className="object-center" />
+          </div>
+          <div className="py-20 flex  ">
+            <Image src='/logo-codo-a-codo.png' alt="logo codo a codo" width={150} height={50} className="object-center" />
+          </div>
+          <div className="py-20 flex  ">
+            <Image src='/logo-silvertech.png' alt="logo silvertech" width={150} height={50} className="object-center" />
+          </div>
+        </div>
       </div>
     </section>
   );
