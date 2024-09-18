b3e43980d2817fab8e5df6ef1bfd9c5e10451ba0
Gabriel Garcia <gabito1966@gmail.com>
2024-09-18 10:08:54 -0300

    Cambio de alumnos por estudiante

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -84,7 +84,7 @@ export default async function Home() {
           <h3 className="font-semibold text-xl mx-6 my-3">Notificaciones</h3>
           <div className="flex p-1 ml-5">
           <svg  xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  className="icon icon-tabler icons-tabler-outline icon-tabler-bell-exclamation"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M15 17h-11a4 4 0 0 0 2 -3v-3a7 7 0 0 1 4 -6a2 2 0 1 1 4 0a7 7 0 0 1 4 6v1.5" /><path d="M9 17v1a3 3 0 0 0 6 0v-1" /><path d="M19 16v3" /><path d="M19 22v.01" /></svg>
-          <p className="text-md ml-1">Alumnos nuevos</p>
+          <p className="text-md ml-1">Estudiantes nuevos</p>
           </div>
           <div className="flex p-1 ml-5">
             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="icon icon-tabler icons-tabler-outline icon-tabler-bell-exclamation"><path stroke="none" d="M0 0h24v24H0z" fill="none" /><path d="M15 17h-11a4 4 0 0 0 2 -3v-3a7 7 0 0 1 4 -6a2 2 0 1 1 4 0a7 7 0 0 1 4 6v1.5" /><path d="M9 17v1a3 3 0 0 0 6 0v-1" /><path d="M19 16v3" /><path d="M19 22v.01" /></svg>
@@ -92,7 +92,7 @@ export default async function Home() {
           </div>
           <div className="flex p-1 ml-5">
           <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="icon icon-tabler icons-tabler-outline icon-tabler-bell-exclamation"><path stroke="none" d="M0 0h24v24H0z" fill="none" /><path d="M15 17h-11a4 4 0 0 0 2 -3v-3a7 7 0 0 1 4 -6a2 2 0 1 1 4 0a7 7 0 0 1 4 6v1.5" /><path d="M9 17v1a3 3 0 0 0 6 0v-1" /><path d="M19 16v3" /><path d="M19 22v.01" /></svg>
-          <p className="text-md ml-1">Alumnos se dieron de baja</p>
+          <p className="text-md ml-1">Estudiantes se dieron de baja</p>
           </div>
           <div className="flex p-1 ml-5">
             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="icon icon-tabler icons-tabler-outline icon-tabler-bell-exclamation"><path stroke="none" d="M0 0h24v24H0z" fill="none" /><path d="M15 17h-11a4 4 0 0 0 2 -3v-3a7 7 0 0 1 4 -6a2 2 0 1 1 4 0a7 7 0 0 1 4 6v1.5" /><path d="M9 17v1a3 3 0 0 0 6 0v-1" /><path d="M19 16v3" /><path d="M19 22v.01" /></svg>
@@ -102,8 +102,8 @@ export default async function Home() {
         <div className="h-48 border-2 m-5 border-slate-600 rounded-lg">
             <h2 className="font-bold text-center text-xl mx-6 my-3">Acciones Rápidas</h2>
           <div className="p-2 flex flex-col justify-center">
-            <Link href='/register/estudiantes' className="bg-blue-400 hover:bg-blue-700 p-2 text-white rounded-full text-center w-40 mx-auto">Registrar Alumno</Link>
-            <Link href='/register/mentores' className="bg-blue-400 hover:bg-blue-700 p-2 text-white rounded-full text-center w-40 mx-auto mt-3">Registrar Mentor</Link>
+            <Link href='/register/estudiantes' className="bg-blue-400 hover:bg-blue-700 p-2 text-white rounded-full text-center min-w-45 mx-auto">Registrar Estudiante</Link>
+            <Link href='/register/mentores' className="bg-blue-400 hover:bg-blue-700 p-2 text-white rounded-full text-center min-w-45 mx-auto mt-3">Registrar Mentor</Link>
           </div>
         </div>
       </section>
