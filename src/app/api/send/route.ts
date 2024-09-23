11613ed61528f77047eb3bef2e8abac13ddaac1b
javier <espindolajavier2013@gmail.com>
2024-09-23 20:57:53 -0300

    se agrego la  libreria de resend, para en el vio del email, se genero el componenete para el envio de email y el metodo post para el envio de email.

---
+++ src/app/api/send/route.ts
@@ -0,0 +1,24 @@
+
+import { EmailTemplate } from '@/components/email/email-template';
+import { Resend } from 'resend';
+
+const resend = new Resend(process.env.RESEND_API_KEY);
+
+export async function POST() {
+  try {
+    const { data, error } = await resend.emails.send({
+      from: 'Polo-IT <onboarding@resend.dev>',
+      to: ['espindolajavier2013@gmail.com'],
+      subject: 'Hello world',
+      react: EmailTemplate({ firstName: 'John' }),
+    });
+
+    if (error) {
+      return Response.json({ error }, { status: 500 });
+    }
+
+    return Response.json(data);
+  } catch (error) {
+    return Response.json({ error }, { status: 500 });
+  }
+}
