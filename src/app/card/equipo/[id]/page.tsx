159ed95136e6ae47df932723ec9e89b8d471194c
Gabriel Garcia <gabito1966@gmail.com>
2024-09-30 11:23:07 -0300

    Cambios en Card equipos

--- src/app/card/equipo/[id]/page.tsx
+++ src/app/card/equipo/[id]/page.tsx
@@ -77,127 +77,101 @@ export default async function EquipoCard({
             <div className="w-full mb-5 border-2 border-gray-200 flex-col p-1 md:p-2 bg-white rounded-lg shadow-md text-black ">
                 <h2 className="text-center font-bold pt-2 pb-5 underline text-xl">Mentores</h2>
             {
-                    <div className="grid grid-cols-2 md:grid-cols-5 ">
-                        <div className="flex-auto">
-                            <div className="ml-16 max-md:ml-0 ">
-                                <h4 className="block mb-1 text-md text-gray-500 font-medium">Nombre:</h4>
-                                <p className="bg-transparent text-base mb-3 font-semibold">{equipo.mentor}</p>
-                            </div>
-                        </div>
+                    <div className="grid grid-cols-1 md:grid-cols-4 ">
                         <div className="flex-auto">
                             <div className="ml-10 max-md:ml-0 ">
-                                <h4 className="block mb-1 text-md text-gray-500 font-medium">Apellido</h4>
-                                <p className="bg-transparent text-base mb-3 font-semibold">{equipo.mentor_apellido}</p>
+                                <h4 className="block mb-1 text-md text-black text-left font-semibold bg-gray-100">Apellido y Nombre</h4>
+                                <p className="bg-transparent text-base mb-3">{equipo.mentor_apellido}, {equipo.mentor}</p>
                             </div>
-                        </div>
+                        </div>                      
                         <div className="flex-auto">
                             <div className="ml-10 max-md:ml-0 ">
-                                <h4 className="block mb-1 text-md text-gray-500 font-medium">Email:</h4>
-                                <p className="bg-transparent text-base mb-3 font-semibold">{equipo.mentor_email}</p>
+                                <h4 className="block mb-1 text-md text-black text-left font-semibold bg-gray-100">Email:</h4>
+                                <p className="bg-transparent text-base mb-3">{equipo.mentor_email}</p>
                             </div>
                         </div>
                         <div className="flex-auto">
                             <div className="ml-16 max-md:ml-0 ">
-                                <h4 className="block mb-1 text-md text-gray-500 font-medium">Teléfono:</h4>
-                                <p className="bg-transparent text-base mb-3 font-semibold">{equipo.mentor_telefono}</p>
+                                <h4 className="block mb-1 text-md text-black text-left font-semibold bg-gray-100">Teléfono:</h4>
+                                <p className="bg-transparent text-base mb-3">{equipo.mentor_telefono}</p>
                             </div>
                         </div>
                         <div className="flex-auto">
                         <div className="ml-16 max-md:ml-0 ">
-                            <h4 className="block mb-1 text-md text-gray-500 font-medium">Mentor</h4>
-                            <p className="bg-transparent text-base mb-3 font-semibold">Tecnologias</p>
+                            <h4 className="block mb-1 text-md text-black text-left font-semibold bg-gray-100">Mentor</h4>
+                            <p className="bg-transparent text-base mb-3">Tecnologias</p>
                         </div>
                     </div>
                     <div className="flex-auto">
-                            <div className="ml-16 max-md:ml-0 ">                              
-                                <p className="bg-transparent text-base mb-3 font-semibold">{equipo.mentor_qa}</p>
-                            </div>
-                        </div>
-                        <div className="flex-auto">
-                            <div className="ml-10 max-md:ml-0 ">
-                                <p className="bg-transparent text-base mb-3 font-semibold">{equipo.mentor_qa_apellido}</p>
+                            <div className="ml-10 max-md:ml-0 ">                              
+                                <p className="bg-transparent text-base mb-3">{equipo.mentor_qa_apellido}, {equipo.mentor_qa}</p>
                             </div>
                         </div>
                         <div className="flex-auto">
                             <div className="ml-10 max-md:ml-0 ">   
-                                <p className="bg-transparent text-base mb-3 font-semibold">{equipo.mentor_qa_email}</p>
+                                <p className="bg-transparent text-base mb-3">{equipo.mentor_qa_email}</p>
                             </div>
                         </div>
                         <div className="flex-auto">
                             <div className="ml-16 max-md:ml-0 ">                           
-                                <p className="bg-transparent text-base mb-3 font-semibold">{equipo.mentor_qa_telefono}</p>
+                                <p className="bg-transparent text-base mb-3">{equipo.mentor_qa_telefono}</p>
                             </div>
                         </div>
                         <div className="flex-auto">
                             <div className="ml-16 max-md:ml-0 ">
-                                
-                                <p className="bg-transparent text-base mb-3 font-semibold">QA</p>
-                            </div>
-                        </div>
-                        <div className="flex-auto">
-                            <div className="ml-16 max-md:ml-0 ">
-                               <p className="bg-transparent text-base mb-3 font-semibold">{equipo.mentor_ux_ui}</p>
+                                <p className="bg-transparent text-base mb-3">QA</p>
                             </div>
                         </div>
                         <div className="flex-auto">
                             <div className="ml-10 max-md:ml-0 ">
-                               <p className="bg-transparent text-base mb-3 font-semibold">{equipo.mentor_ux_ui_apellido}</p>
+                               <p className="bg-transparent text-base mb-3">{equipo.mentor_ux_ui_apellido}, {equipo.mentor_ux_ui}</p>
                             </div>
                         </div>
+                        
                         <div className="flex-auto">
                             <div className="ml-10 max-md:ml-0 ">
 
-                                <p className="bg-transparent text-base mb-3 font-semibold">{equipo.mentor_ux_ui_email}</p>
+                                <p className="bg-transparent text-base mb-3">{equipo.mentor_ux_ui_email}</p>
                             </div>
                         </div>
                         <div className="flex-auto">
                             <div className="ml-16 max-md:ml-0 ">
-                                <p className="bg-transparent text-base mb-3 font-semibold">{equipo.mentor_ux_ui_telefono}</p>
+                                <p className="bg-transparent text-base mb-3 ">{equipo.mentor_ux_ui_telefono}</p>
                             </div>
                         </div>
                         <div className="flex-auto">
                             <div className="ml-16 max-md:ml-0 ">
-                                <p className="bg-transparent text-base mb-3 font-semibold">UX/UI</p>
+                                <p className="bg-transparent text-base mb-3">UX/UI</p>
                             </div>
                         </div>
                     </div>
                 }
             </div>
                 <div className="w-full  max-h-124 flex-col p-1  md:p-2 text-black border-2 border-gray-200 bg-white rounded-lg shadow-md">
-                <h2 className="text-center font-bold pt-2 pb-5 underline text-xl">Integrantes</h2>
+                <h2 className="text-center font-bold pt-2 pb-4 underline text-xl">Integrantes</h2>
                     {
-                    <div className="grid grid-cols-1 md:grid-cols-5 ">
-                        <div className="flex-auto">
-                            <div className="ml-16 max-md:ml-0 ">
-                                <h4 className="block mb-2 text-md text-gray-500 font-medium">Nombre:</h4>
-                                <p className="bg-transparent text-base mb-3 font-semibold">Nombre del estudiante</p>
-                            </div>
-                        </div>
-                        <div className="flex-auto">
-                            <div className="ml-10 max-md:ml-0 ">
-                                <h4 className="block mb-2 text-md text-gray-500 font-medium">Apellido</h4>
-                                <p className="bg-transparent text-base mb-3 font-semibold">Apellido del estudiante</p>
-                            </div>
-                        </div>
-                        <div className="flex-auto">
-                            <div className="ml-10 max-md:ml-0 ">
-                                <h4 className="block mb-2 text-md text-gray-500 font-medium">Email:</h4>
-                                <p className="bg-transparent text-base mb-3 font-semibold">Email del estudiante</p>
-                            </div>
-                        </div>
-                        <div className="flex-auto">
-                            <div className="ml-16 max-md:ml-0 ">
-                                <h4 className="block mb-2 text-md text-gray-500 font-medium">Teléfono:</h4>
-                                <p className="bg-transparent text-base mb-3 font-semibold">Telefono del estudiante</p>
-                            </div>
-                        </div>
-                        <div className="flex-auto">
-                            <div className="ml-16 max-md:ml-0 ">
-                                <h4 className="block mb-2 text-md text-gray-500 font-medium">Tecnologia:</h4>
-                                <p className="bg-transparent text-base mb-3 font-semibold">Tecnologia del estudiante</p>
-                            </div>
-                        </div>
-                    </div>
+                   <div className="overflow-x-auto">
+                   <table className="table-auto w-full">
+                     <thead className="text-left bg-gray-100">
+                       <tr>
+                         <th className="px-4 py-2">Apellido y Nombre</th>
+                         <th className="px-4 py-2">Email</th>
+                         <th className="px-4 py-2">Teléfono</th>
+                         <th className="px-4 py-2">Tecnología</th>
+                       </tr>
+                     </thead>
+                     <tbody>
+                       {equipo.nombres_estudiantes.map((e:any, i:number) => (
+                         <tr key={`${e}-${i}`} className="hover:bg-gray-100">
+                           <td className="px-4 py-2">{equipo.apellidos_estudiantes[i]}, {e} </td>
+                           <td className="px-4 py-2">{equipo.emails_estudiantes[i]}</td>
+                           <td className="px-4 py-2">{equipo.telefonos_estudiantes[i]}</td>
+                           <td className="px-4 py-2">{equipo.tecnologias[i]}</td>
+                         </tr>
+                       ))}
+                     </tbody>
+                   </table>
+                 </div>
                     }
                 </div>
                 </div>
