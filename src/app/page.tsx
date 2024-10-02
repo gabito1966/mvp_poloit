8e89b3e5d4ed73bcf784e986683ffdaab3c73128
Gabriel Garcia <gabito1966@gmail.com>
2024-10-02 10:30:06 -0300

    Ajustes varios

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+'use client'
 import Carousel from "@/components/Carousel";
 import ChartAlumnosTecnologias from "@/components/ChartAlumnosTecnologias";
 import HomeCardWrapper from "@/components/HomeCardWrapper";
@@ -24,8 +25,11 @@ export default async function Home() {
   ];
 
   return (
-    <main className=" w-full p-6 max-w-screen-2xl">
-      <h1 className="sm:text-4xl font-bold mb-8 text-center lg:text-left  text-2xl">Inicio</h1>
+    
+    <main className=" w-full p-6 max-w-screen-2xl dark:bg-gray-950">
+      <header className="flex justify-between">
+        <h1 className="sm:text-4xl mb-6 font-bold text-2xl">Inicio</h1>
+      </header>
       <div className="w-full  gap-6 items-center justify-between grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 P-4 h-full sm:items-center sm:m-auto ">
         <Suspense fallback={<CardsWrapperSkeleton />}>
           <HomeCardWrapper />
@@ -60,5 +64,6 @@ export default async function Home() {
 
       </section>
     </main>
+
   );
 }
