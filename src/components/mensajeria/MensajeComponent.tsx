8a32e9fc0ee6450324d4acb89903d921584e721c
Gabriel Garcia <gabito1966@gmail.com>
2024-10-02 21:01:16 -0300

    Responsividad del MensajeComponent

--- src/components/mensajeria/MensajeComponent.tsx
+++ src/components/mensajeria/MensajeComponent.tsx
@@ -120,11 +120,11 @@ export default function MensajeComponent() {
     <>
       <div className="flex flex-col w-full h-full">
         <div className="flex w-full ">
-          <h1 className="text-3xl font-bold ml-12">Mensajería</h1>
+          <h1 className="text-3xl font-bold ml-12 text-center md:text:left">Mensajería</h1>
         </div>
 
         <div className="grid grid-cols-5 gap-4 px-10 py-4 ">
-          <div className="col-span-2 m-2 px-7 py-5 bg-white text-black rounded-lg shadow-lg ">
+          <div className="col-span-5 md:col-span-2 m-2 px-7 py-5 bg-white text-black rounded-lg shadow-lg ">
             <div className="py-2 flex gap-2 justify-left items-center">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
@@ -153,7 +153,7 @@ export default function MensajeComponent() {
             </div>
           </div>
 
-          <div className="col-span-3 m-2 px-5 pt-4 pb-8 bg-white  text-black rounded-lg shadow-lg">
+          <div className="col-span-5 md:col-span-3 min-h-[800px] m-2 px-5 pt-4 pb-8 bg-white  text-black rounded-lg shadow-lg">
             <div className="flex items-center justify-between py-2">
               <div className="flex flex-row justify-center items-center gap-2">
                 <svg
@@ -167,10 +167,10 @@ export default function MensajeComponent() {
                   <path d="M2 22C2 17.5817 5.58172 14 10 14C14.4183 14 18 17.5817 18 22H16C16 18.6863 13.3137 16 10 16C6.68629 16 4 18.6863 4 22H2ZM10 13C6.685 13 4 10.315 4 7C4 3.685 6.685 1 10 1C13.315 1 16 3.685 16 7C16 10.315 13.315 13 10 13ZM10 11C12.21 11 14 9.21 14 7C14 4.79 12.21 3 10 3C7.79 3 6 4.79 6 7C6 9.21 7.79 11 10 11ZM18.2837 14.7028C21.0644 15.9561 23 18.752 23 22H21C21 19.564 19.5483 17.4671 17.4628 16.5271L18.2837 14.7028ZM17.5962 3.41321C19.5944 4.23703 21 6.20361 21 8.5C21 11.3702 18.8042 13.7252 16 13.9776V11.9646C17.6967 11.7222 19 10.264 19 8.5C19 7.11935 18.2016 5.92603 17.041 5.35635L17.5962 3.41321Z"></path>
                 </svg>
                 <div className=" flex flex-col">
-                  <h2 className="text-xl font-semibold flex gap-2">
+                  <h2 className="text-md md:text-xl font-semibold flex gap-2">
                     Equipo 3{" "}
                   </h2>
-                  <p className="text-gray-700">Integrantes:</p>
+                  <p className="text-gray-700 text-sm md:text-md">Integrantes:</p>
                 </div>
               </div>
 
@@ -188,7 +188,7 @@ export default function MensajeComponent() {
                   >
                     <path d="M11.1244 1.09094H12.8753L12.9269 1.9453C13.2227 6.85075 17.1493 10.7773 22.0546 11.0732L22.909 11.1247V12.8757L22.0546 12.9272C17.1493 13.2231 13.2227 17.1498 12.9269 22.0551L12.8753 22.9095H11.1244L11.0728 22.0551C10.777 17.1498 6.85036 13.2231 1.94518 12.9272L1.09082 12.8757V11.1247L1.94518 11.0732C6.85036 10.7773 10.777 6.85075 11.0728 1.9453L11.1244 1.09094ZM11.9999 5.85023C10.83 8.61547 8.61512 10.8304 5.84996 12.0002C8.61512 13.1701 10.83 15.385 11.9999 18.1502C13.1697 15.385 15.3846 13.1701 18.1498 12.0002C15.3846 10.8304 13.1697 8.61547 11.9999 5.85023Z"></path>
                   </svg>
-                  <span className="text-center text-md ">
+                  <span className="text-center text-sm md:text-md ">
                     {" "}
                     Generar con IA
                   </span>
@@ -218,7 +218,6 @@ export default function MensajeComponent() {
             <div className="h-3/4 border-gray-100 border-t">
               {iaFormState ? typingEffectValue : form.mensaje}
             </div>
-
             <form
               onSubmit={handleSubmit}
               className=" border-gray-100 border-t w-full pt-5  flex items-center gap-4 "
