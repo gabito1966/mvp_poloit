41f5e2a4216689a302e125cc076a9847249d634c
Gabriel Garcia <gabito1966@gmail.com>
2024-10-03 14:56:14 -0300

    Logo mobile

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -2,8 +2,8 @@
 import { roboto } from "@/lib/fonts";
 import { signOut, singIn } from "@/lib/server/session";
 import clsx from "clsx";
-import Image from "next/image";
 import LinkSideBar from "./LinkSideBar";
+import ResponsiveImage from "./ResponsiveImage";
 
 
 const Sidebar: React.FC = () => {
@@ -118,22 +118,7 @@ const Sidebar: React.FC = () => {
         <ul className="space-y-4 p-2 lg:p-6">
           <li >
               <div className="flex justify-center rounded-sm">
-              <Image 
-                src="/logo-polo-it.png" 
-                alt="Logo de Polo It"       
-                objectFit="cover"
-                className="hidden lg:block" 
-                width={150} 
-                height={50}
-              />
-               <Image
-                src="/polo-logo-mobile.png" 
-                alt="Logo de Polo It mobile"
-                objectFit="cover"
-                className="block lg:hidden"
-                width={90} 
-                height={60}
-              />
+                <ResponsiveImage />
               </div>
           </li>
           
