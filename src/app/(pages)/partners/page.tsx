d9b8d100f5685d3a73bbaccc908d6f1f210a7aaa
Gabriel Garcia <gabito1966@gmail.com>
2024-10-10 12:35:18 -0300

    Agregado de logo de open computacion

--- src/app/(pages)/partners/page.tsx
+++ src/app/(pages)/partners/page.tsx
@@ -46,6 +46,7 @@ export default function Partners() {
         "/logo-softtek.png",
         "/logo-pigmalion.png",
         "/logo-mindhub.png",
+        "/logo-open-computacion.png",
     ];
 
     return (
